a651be037ad614ca533ad15bbc41f51f1dac432e
ec429 <ec429@cam.ac.uk>
2012-02-12 21:45:39 +0000

    Improved the shellout filechooser

--- filechooser.c
+++ filechooser.c
@@ -28,13 +28,9 @@ int main(int argc, char *argv[])
 	g_signal_connect(mainwindow, "destroy", G_CALLBACK(destroy), NULL);
 	gtk_window_set_title(GTK_WINDOW(mainwindow), "Spiffy - Load file");
 	gtk_window_maximize(GTK_WINDOW(mainwindow));
-	GtkWidget *box1 = gtk_vbox_new(false, 0);
-	gtk_container_add(GTK_CONTAINER(mainwindow), box1);
 	GtkWidget *chooser = gtk_file_chooser_widget_new(GTK_FILE_CHOOSER_ACTION_OPEN);
-	gtk_box_pack_start(GTK_BOX(box1), chooser, true, true, 0);
-	GtkWidget *openbutton = gtk_button_new_with_label("Load file");
-	g_signal_connect(openbutton, "clicked", G_CALLBACK(clicked), chooser);
-	gtk_box_pack_start(GTK_BOX(box1), openbutton, false, false, 0);
+	g_signal_connect(chooser, "file-activated", G_CALLBACK(clicked), chooser);
+	gtk_container_add(GTK_CONTAINER(mainwindow), chooser);
 	gtk_widget_show_all(mainwindow);
 	gtk_main();
 	return(0);
