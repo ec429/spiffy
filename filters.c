8762b50dd72987a54abadf2119c69c86d0dc6a99
ec429 <ec429@cam.ac.uk>
2012-05-31 12:17:03 +0100

    filters: improved the PAL filter
    The current code gets the right results, but it doesn't make sense;
     effectively I have the Cb carrier at twice the freq. of the Cr.  I suppose it
     could be some kind of strange mixing effect.  Anyway, the point is, I don't
     understand PAL well enough to know why this code works, but I kept tweaking it
     and this is what I ended up with.

--- filters.c
+++ filters.c
@@ -27,7 +27,6 @@ void filter_pix(unsigned int filt_mask, unsigned int x, unsigned int y, unsigned
 	static unsigned char rowr[320], rowg[320], rowb[320];
 	static unsigned char old[320][296][3];
 	static signed int palcr, palcb;
-	static bool oddfield=false;
 	
 	if(filt_mask&FILT_BW)
 	{
@@ -36,17 +35,14 @@ void filter_pix(unsigned int filt_mask, unsigned int x, unsigned int y, unsigned
 	
 	if((filt_mask&FILT_PAL)&&!(filt_mask&FILT_BW)) // PAL doesn't make sense for a BW set
 	{
-		oddfield=!oddfield;
 		unsigned char luma=(*r+*g+*b)/3;
 		signed int cb=*b-luma, cr=*r-luma;
-		unsigned char sc=((x*5)&4)>>1, cc=(((x+2)*5)&4)>>1;
-		palcr=(palcr>>1)+(palcr>>2)+((sc-1)*(luma-127)>>2);
-		palcb=(palcb>>1)+(palcb>>2)+((cc-1)*(luma-127)>>2);
-		*r=min(max(luma+cr+palcr, 0), 255);
-		*b=min(max(luma+cb+((y&1)?-palcb:palcb), 0), 255);
+		unsigned char sc=(x&4)>>1, cc=x&2;
+		palcr=(palcr>>1)+(palcr>>2)+((sc-1)*(luma-127)>>1);
+		palcb=(palcb>>1)+(palcb>>2)+((cc-1)*(luma-127)>>1);
+		*r=min(max(luma+cr+((y&1)?palcr:-palcr), 0), 255);
+		*b=min(max(luma+cb+((y&1)?palcb:-palcb), 0), 255);
 		*g=min(max(luma*3-*r-*b, 0), 255);
-		/*palf=(abs(luma-lumo)>>2)+(palf>>1)+(palf>>2);
-		*r=palf^(((y^x)&1)?0xff:0)^((x&4)<<5); // not anything like what actually happens; just for testing*/
 	}
 	
 	if(filt_mask&FILT_SCAN)
